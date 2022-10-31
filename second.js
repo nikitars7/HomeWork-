b136820d783bc5211a6afd9a0821c4fd8b0223c4
Nikita <nikita_rs7@ukr.net>
2022-10-31 16:02:15 +0200

    rr

--- second.js
+++ second.js
@@ -1,5 +1,20 @@
-let arr = [5, 2, 1, -10, 8];
+let arr = [5, 2, 1, -10, 8,];
 
-let result = arr.sort((a, b) => b - a);
+// let result = arr.sort((a, b) => b - a);
 
-alert(arr);
+// alert(arr);
+
+function compare(a, b) {
+   if (a > b) {
+      return -1;
+   } else if (a < b) {
+      return 1;
+   } else {
+      return 0;
+   }
+}
+let result2 = arr.sort(compare);
+
+alert(result2);
+
+
